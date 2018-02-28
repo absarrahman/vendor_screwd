32c22be13f98a52d2f743294bb49dc6b37433352
absarrahman <raman040635@gmail.com>
2018-02-28 16:22:21 +0000

    Forgot to mention some paths which did not let me compile the rom

---
+++ products/screwd_kenzo.mk
@@ -0,0 +1,54 @@
+#
+# Copyright 2016 The CyanogenMod Project Copyright 2017 The LineageOS Project 
+# Copyright 2017 The Cardinal-AOSP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
+
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_ARCORE := true
+$(call inherit-product, vendor/screwd/configs/caf_required.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+ifeq (screwd_kenzo,$(TARGET_PRODUCT))
+
+# Include Screw'd common configuration
+include vendor/screwd/main.mk
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := kenzo
+PRODUCT_NAME := screwd_kenzo
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 3
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys \
+    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.2.1.0.MHOCNDL release-keys"
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.screwd.device.short=$(SCREWD_DEVICE_SHORT) \
+
+endif
